ea282739678389a98832845a89c95af5cd89904a
sanchitgoel01 <sgoel008@ucr.edu>
2020-11-19 13:37:35 -0800

    Create basic BaseFactory class

---
+++ BaseFactory.cpp
@@ -0,0 +1,50 @@
+#include "BaseFactory.h"
+#include "op.hpp"
+#include "mult.hpp"
+
+Base * parseOperator(char c, Base* op1, Base* op2) {
+    // TODO Fill this out
+    if (c == '*')
+        return new Mult(op1, op2);
+}
+
+bool isOperator(char c) {
+    // TODO Finish this
+    return c == '*' || c == '-';
+}
+
+bool isNumber(char c) {
+    // FIXME STUB
+    return true;
+}
+
+Base * BaseFactory::parse(char **input, int length) {
+    char* charArray = input[0];
+
+    Base* lastOperand = nullptr;
+    char lastOperator = 0;
+
+    for (int i = 0; charArray[i] != 0; ++i) {
+        char c = charArray[i];
+
+        if (isOperator(c)) {
+            lastOperator = c;
+        }
+        else if (isNumber(c)) {
+            // Convert the character to a number
+            int val = c - '0';
+
+            Op* op = new Op(val);
+
+            if (lastOperator) {
+                lastOperand = parseOperator(lastOperator, lastOperand, op);
+                lastOperator = 0;
+            }
+            else {
+                lastOperand = op;
+            }
+        }
+    }
+
+    return lastOperand;
+}
